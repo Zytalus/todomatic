cc9d297520972b3332ab16758e61423ddbee94a1
Nicholas Deckard <nicholas.deckard@crazymagic.studio>
2022-10-07 20:42:49 -0500

    refactor todo data

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const DATA = [
+  { id: "todo-0", name: "eat", completed: true },
+  { id: "todo-1", name: "code", completed: false },
+  { id: "todo-2", name: "sleep", completed: false },
+];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>
 );
